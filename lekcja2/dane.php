0d679166a0934e14b1d95ade38c46dfea8bc0dfc
Mikołaj Woźniak <inkubator@mozesz.pl>
2018-03-26 12:07:58 +0200

    Add code from lesson2 and lesson3

---
+++ lekcja2/dane.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$dane = [
+    ["nazwa" => "capricciossa", "opis" => "ciasto, ser, kabanos"],
+    ["nazwa" => "margarita", "opis" => "ciasto, ser, kabanos, cebula"],
+    ["nazwa" => "hawai", "opis" => "ciasto, kabanos, cebula"],
+    ["nazwa" => "mafioso", "opis" => "ciasto, kabanos, cebula"],
+];
+
