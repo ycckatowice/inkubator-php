bafe0d552bd7657658f288d87fd4597678d4e414
Krzysiu <Krzysiu@Krzysiek>
2018-03-27 19:25:42 +0200

    super zaawansowany algorytm

--- lekcja4/order.php
+++ lekcja4/order.php
@@ -1,6 +1,13 @@
 <?php
 
 include_once 'partial/header.php';
+
+if(isset($_GET['order_id'])){
+    $orderid= $_GET['order_id'];
+    
+}else{
+    $orderid = '';
+}
 ?>
 <div class="container">
     <h1>Zamawiasz pizze</h1>
