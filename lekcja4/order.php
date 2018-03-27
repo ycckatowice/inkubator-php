36b98f60476064da28fc53f997aa8a708b79e215
1 <1@Asus>
2018-03-27 19:26:13 +0200

    add order id

--- lekcja4/order.php
+++ lekcja4/order.php
@@ -1,6 +1,12 @@
 <?php
 
 include_once 'partial/header.php';
+if(isset($_GET['order_id'])){
+$orderId=$_GET['order_id'];
+}else{
+    $orderId='';
+}
+
 ?>
 <div class="container">
     <h1>Zamawiasz pizze</h1>
