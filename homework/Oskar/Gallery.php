7a86702755ac6632eb8b0c8b91ebbb0f6644fa29
OSKAR <OSKAR@DESKTOP-FU3RF1T>
2018-03-29 00:21:47 +0200

    add homework.

---
+++ homework/Oskar/Gallery.php
@@ -0,0 +1,78 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$gallery = [ 
+    [   "id" => 0,
+        
+        "img" => "img/breakingbad.jpg",
+    ],
+    [   "id" => 1,
+        
+        "img" => "img/fringe.jpg",
+    ],
+    [
+        "id" => 2,
+        "img" => "img/got.jpg",
+    ],
+    [   "id" => 3,
+        
+        "img" => "img/wikingowie.jpg",
+    ],
+    [   "id" => 4,
+        
+        "img" => "img/fringe.jpg",
+    ],
+    [   "id" => 5,
+      
+        "img" => "img/breakingbad.jpg",
+    ],
+    [   "id" => 6,
+        
+        "img" => "img/fringe.jpg",
+    ],
+    [   "id" => 7,
+        
+        "img" => "img/wikingowie.jpg",
+    ],
+    [   "id" => 8,
+        
+        "img" => "img/got.jpg",
+    ],
+    [   "id" => 9,
+        
+        "img" => "img/breakingbad.jpg",
+    ],
+    [   "id" => 10,
+        
+        "img" => "img/fringe.jpg",
+    ],
+    [   "id" => 11,
+     
+        "img" => "img/got.jpg",
+    ],
+    [   "id" => 12,
+        
+        "img" => "img/wikingowie.jpg",
+    ],
+    [   "id" => 13,
+       
+        "img" => "img/breakingbad.jpg",
+    ],
+    [   "id" => 14,
+        
+        "img" => "img/fringe.jpg",
+    ],
+    [   "id" => 15,
+    
+        "img" => "img/got.jpg",
+    ],
+   
+    
+];
+
---
+++ homework/Oskar/ZadDom.php
@@ -0,0 +1,92 @@
+<?php
+if(file_exists("Gallery.php"))
+ {
+require 'Gallery.php';
+ }else 
+ {
+     exit("<h1>something wrong</h1>");
+ }
+ 
+ ?>
+<html> 
+
+    <head>
+        
+    <title>My Gallery</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        .container
+        {
+             background-color:#343a40;
+             max-width:inherit;
+        }
+        .kolumna
+        {
+            
+        }
+        .card-img-top
+        {
+            width:23%;
+            height:23%;
+            margin:1% 1% 0.5% 0.5%;
+            border-radius:10px;
+        }
+      .header img
+      {
+          width:1346px;
+      }
+    </style>
+    
+    <body>
+        
+       
+            <div class="header">
+                <?php 
+                if(isset($_GET['id'])) 
+                {
+                    $id = $_GET['id'];
+                    for($i = 0; $i <=15; $i++)
+                    {
+                    
+                        if($id == $i)
+                        {
+                            echo '<a href="ZadDom.php"><img src="'.$gallery[$id]['img'].'" class="img-fluid" alt="Responsive image"/></a> ';
+                        } 
+                    }
+                    
+                  }else 
+                  {
+                      echo '<a href ="ZadDom.php"><img src="'.$gallery[rand(0,count($gallery)-1)]['img'].'" class="img-fluid"/></a> ';
+                      
+                  }     
+                  ?>
+            </div>
+       
+            <div class="container">
+                <div class="kolumna">
+                
+                     <div class="box">
+                         <?php
+                         for( $i = 0; $i < count($gallery); $i++)
+                         {
+                            echo '<a href ="ZadDom.php?id='.$gallery[$i]['id'].'"><img src="'.$gallery[$i]['img'].'" class="card-img-top  "/></a> ';
+                         }
+                         ?>
+          
+                    </div>
+                </div>
+            </div>
+        
+        
+    </body>   
+        
+        
+    </head>
+
+
+
+</html>
