131553dfe6abd02603ad90cc1c4b2dd68a6bf9ef
tuki1 <tuki1@DESKTOP-8UP7IKR>
2018-04-01 23:47:50 +0200

    index

---
+++ homework/Łukasz/index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Wojna</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      
+  </body>
+  
+  
+</html>
