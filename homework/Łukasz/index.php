1670cdfd7c7b4b4dca071982295ab8d88cf2de42
tuki1 <tuki1@DESKTOP-8UP7IKR>
2018-04-03 19:04:26 +0200

    Draw mechanics

--- homework/Łukasz/index.php
+++ homework/Łukasz/index.php
@@ -1,51 +1,142 @@
 <?php
 
-    session_start();
+session_start();
 
-    define("BASE_POINTS", 0);
+define("BASE_POINTS", 0);
 
-    if(!isset($_SESSION["myPoints"]) || !isset($_SESSION["compPoints"])) {
-        game();
-    }
+if(!isset($_SESSION["myPoints"]) || !isset($_SESSION["compPoints"])) {
+    game();
+}
+
+if (!isset($compDeck) || !isset($userDeck)){
     require_once "data.php";
-
     $deck = json_encode($data);
+
     file_put_contents("deck.txt", $deck);
 
-    function game()
-    {
-        $_SESSION["myPoints"] = BASE_POINTS;
-        $_SESSION["compPoints"] = BASE_POINTS;
+    $update = file_get_contents("deck.txt");
+    $compDeck = json_decode($update, true);
+    $userDeck = json_decode($update, true);
+}
+
+function game()
+{
+    $_SESSION["myPoints"] = BASE_POINTS;
+    $_SESSION["compPoints"] = BASE_POINTS;
+}
+
+function removeComp($array, $index){
+    if(file_exists("compDeck.txt")){
+        $update = file_get_contents("compDeck.txt");
+        $array = json_decode($update, true);
+    }else{$update = file_get_contents("deck.txt");
+        $array = json_decode($update, true);}
+
+    if(sizeof($array) <=1){
+        echo "Koniec kart. Przegrana.";
+        $update = file_get_contents("deck.txt");
+        $array = json_decode($update, true);
     }
 
-    if(isset($_GET['choice']) && $_GET['choice'] === "play"){
-        $dataSize = sizeof($data);
-        $computerChoice = $data[rand(0, $dataSize - 1)];
-        $userChoice = $data[rand(0, $dataSize -1)];
+    array_splice($array,$index, 1);
+    //unset($array[$index]);
+    var_dump($array);
+    $arr = json_encode($array);
+    file_put_contents("compDeck.txt", $arr);
+}
 
-        $computerScore = $computerChoice['strength'];
-        $userScore = $userChoice['strength'];
+function removeUser($array, $index){
+    if(file_exists("userDeck.txt")){
+        $update = file_get_contents("userDeck.txt");
+        $array = json_decode($update, true);
+    }else{$update = file_get_contents("deck.txt");
+        $array = json_decode($update, true);}
 
-        if($computerChoice['strength'] > $userChoice['strength']){
-            echo "Komputer wygrywa potyczkę <br>";
-            $_SESSION["compPoints"] += $computerScore + $userScore;
-        }else if ($computerChoice === $userChoice){
-            echo"Wojna!";
-        }else{
-            echo "Gracz wygrywa potyczkę <br>";
-            $_SESSION["myPoints"] += $userScore + $computerScore;
-        }
+    if(sizeof($array) <=1){
+        echo "Koniec kart. Przegrana.";
+        $update = file_get_contents("deck.txt");
+        $array = json_decode($update, true);
+    }
 
+    array_splice($array,$index, 1);
+    //unset($array[$index]);
+    var_dump($array);
+    $arr = json_encode($array);
+    file_put_contents("userDeck.txt", $arr);
+}
 
-        if($_GET['choice'] == 'end'){
-            header("Location: index.php");
-        }
+if(isset($_GET['choice']) && $_GET['choice'] === "play"){
 
+    if(file_exists("compDeck.txt")){
+        $update = file_get_contents("compDeck.txt");
+        $compDeck = json_decode($update, true);
+    } else{
+        $update = file_get_contents("deck.txt");
+        $compDeck = json_decode($update, true);
+    }
+
+    if(file_exists("userDeck.txt")){
+        $update = file_get_contents("userDeck.txt");
+        $userDeck = json_decode($update, true);
     }else{
-        game();
-        $computerChoice ="";
-        $userChoice ="";
+        $update = file_get_contents("deck.txt");
+        $userDeck = json_decode($update, true);
     }
+
+    $compDeckSize = sizeof($compDeck);
+    $i = rand(0, $compDeckSize -1);
+    $computerChoice[$i] = $compDeck[$i];
+
+    $userDeckSize = sizeof($userDeck);
+    $j = rand(0, $userDeckSize -1);
+    $userChoice[$j] = $userDeck[$j];
+
+    var_dump($computerChoice[$i]['strength']);
+    var_dump($userChoice[$j]['strength']);
+    $computerScore = $computerChoice[$i]['strength'];
+    $userScore = $userChoice[$j]['strength'];
+
+    $compDeckSize -= 1;
+    $userDeckSize -= 1;
+    echo "pozostałe karty: ".$compDeckSize."<br>";
+    echo "pozostałe karty: ".$userDeckSize."<br>";
+
+    if($computerChoice[$i]['strength'] > $userChoice[$j]['strength']){
+
+        echo "Komputer wygrywa potyczkę <br>";
+        $_SESSION["compPoints"] += $computerScore + $userScore;
+
+        removeComp($compDeck, $i);
+        removeUser($userDeck, $j);
+
+    }else if ($computerChoice === $userChoice){
+        echo"Wojna!";
+        removeComp($compDeck, $i);
+        removeUser($userDeck, $j);
+    }else{
+        echo "Gracz wygrywa potyczkę <br>";
+        $_SESSION["myPoints"] += $userScore + $computerScore;
+        removeComp($compDeck, $i);
+        removeUser($userDeck, $j);
+    }
+
+
+    if($_GET['choice'] === "end"){
+        header("Location: index.php");
+    }
+
+}else{
+    game();
+    $computerChoice ="";
+    $userChoice ="";
+    if(file_exists("compDeck.txt") || file_exists("userDesk.txt")){
+        unlink("compDeck.txt");
+        unlink("userDeck.txt");
+    }
+
+}
+
+
 
 if($_SESSION["compPoints"] >= 100){
     echo "<br><h1>Komputer wygrywa wojnę</h1>";
@@ -61,7 +152,7 @@ if($_SESSION["myPoints"] >= 100){
 
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -71,31 +162,31 @@ if($_SESSION["myPoints"] >= 100){
 
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
+</head>
+<body>
 
-    <div>
-        Komputer: <?php
-        echo $_SESSION["compPoints"];
-        var_dump($computerChoice);
+<div>
+    Komputer: <?php
+    echo $_SESSION["compPoints"];
+    var_dump($computerChoice);
 
-        ?>
-    </div>
+    ?>
+</div>
 
-    <div>
-        Gracz: <?php
-        echo $_SESSION["myPoints"];
-        var_dump($userChoice); ?>
-    </div>
+<div>
+    Gracz: <?php
+    echo $_SESSION["myPoints"];
+    var_dump($userChoice); ?>
+</div>
 
-    <div>
-        <a href="?choice=play">Graj</a>
-    </div>
-    <div>
-        <a href="?choice=end">Zakończ</a>
-    </div>
+<div>
+    <a href="?choice=play">Graj</a>
+</div>
+<div>
+    <a href="?choice=end">Zakończ</a>
+</div>
 
-  </body>
-  
-  
+</body>
+
+
 </html>
