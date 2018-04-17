9129b4ad7d079f384327b7fff8156069d71b716e
Oskar <oskar98116@gmail.com>
2018-04-17 18:40:33 +0200

    Added view.php

--- lekcja8/category/view.php
+++ lekcja8/category/view.php
@@ -28,40 +28,8 @@ if(!$category){
         <td><?= $category['id'] ?></td>
     </tr>
     <tr>
-        <td>Elektronika</td>
-        <td><?= $category['Elektronika'] ?></td>
-    </tr>
-    <tr>
-        <td>Moda</td>
-        <td><?= $category['Moda'] ?></td>
-    </tr>
-    <tr>
-        <td>Dom i Ogród</td>
-        <td><?= $category['Dom i ogród'] ?></td>
-    </tr>
-    <tr>
-        <td>Supermarket</td>
-        <td><?= $category['Supermarket'] ?></td>
-    </tr>
-    <tr>
-        <td>Dziecko</td>
-        <td><?= $category['Dziecko']?></td>
-    </tr>
-    <tr>
-        <td>Uroda i zdrowie</td>
-        <td><?= $category['Uroda i zdrowie']?></td>
-    </tr>
-    <tr>
-        <td>Kultura i rozrywka</td>
-        <td><?= $category['Kultura i rozrywka']?></td>
-    </tr>
-    <tr>
-        <td>Sport i wypoczynek</td>
-        <td><?= $category['Sport i wypoczynek']?></td>
-    </tr>
-    <tr>
-        <td>Motoryzacja</td>
-        <td><?= $category['Motoryzacja']?></td>
+        <td>Name</td>
+        <td><?= $category['name'] ?></td>
     </tr>
 </table>
 
