3d6711bbf19ca271dac7ee614a51507c18e5b811
00118 <00118@DESKTOP-0UFQMU5>
2018-04-17 16:57:06 +0200

    make up

--- lekcja8/category/all.php
+++ lekcja8/category/all.php
@@ -1,8 +1,37 @@
 <?php
+require_once '../partial_view/header.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$categories = categoryFindAll($pdo);
+
+//Find all users here
+?>
+<br><br><br>
+<h1>Category list:</h1>
+<div>
+    <a class="btn btn-success float-right" href='create.php'>Create Category</a>
+    <br><br><br><br>
+</div>
+<table class="table">
+    <tr>
+        <td>Category</td>
+    </tr>
+    <?php
+    // Iterate all users
+    foreach ($categories as $category) {
+        echo "<tr>
+     <td>{$category['name']}</td>
+        <td>
+         <a href='view.php?id={$category['name']}'>View</a>
+         <a href='update.php?id={$category['name']}'>Update</a>
+         <a href='delete.php?id={$category['name']}'>Delete</a>
+       </td>
+       </tr>";
+    }
+    // Add actions to each row
+    ?>
+</table>
+
+<?php
+require_once '../partial_view/footer.php';
+
 
