f4c7a50ac2b86b5d6152d832065561039706c848
1 <1@Asus>
2018-04-24 19:32:29 +0200

    add order id

--- lekcja8/product/view.php
+++ lekcja8/product/view.php
@@ -1,8 +1,56 @@
 <?php
-// TOMEK
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+//TOMEK
+require_once '../partial_view/header.php';
+
+// getRequestGetVariable id
+$id = getRequestGetVariable('id');
+// if not set redirect:  /lekcja8/product/all.php?selectId
+if(!$id){
+    header('Location: /lekcja8/product/all.php?selectId');
+}
+
+//productFindOneById
+
+
+
+$productRepository = new ProductRepository($pdo, (int) $id);
+    
+
+$product = $productRepository->findOneById((int) $id);
+
+//$product = ($pdo, (int) $id);
+// if no user redirect: /lekcja8/product/all.php?productNotExistsId=' . $id
+
+if(!$produkt){
+   header('Location: /lekcja8/product/all.php?productNotExistsId=' . $id);
+}
+
+
+?>
+<h1>View product: <?= $id ?></h1>
+
+
+<table class="table">
+    <tr>
+        <td>Id</td>
+        <td><?= $product->id ?></td>
+    </tr>
+    <tr>
+        <td>Product name</td>
+        <td><?= $product->name ?></td>
+    </tr>
+    <tr>
+        <td>Category id</td>
+        <td><?= $product->category_id ?></td>
+    </tr>
+    <tr>
+        <td>Cost</td>
+        <td><?= $product->cost ?></td>
+    </tr>
+</table>
+
+
+
+<?php
+require_once '../partial_view/footer.php';
